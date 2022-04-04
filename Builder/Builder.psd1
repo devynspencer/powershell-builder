40ac8fba57e6d62d536266deb701c26cdea4781f
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-04-03 17:09:22 -0700

    Add Build-Module function

--- Builder/Builder.psd1
+++ Builder/Builder.psd1
@@ -3,7 +3,7 @@
     RootModule = 'Builder.psm1'
 
     # Version number of this module.
-    ModuleVersion = '0.0.0'
+    ModuleVersion = '0.1.0'
 
     # Supported PSEditions
     # CompatiblePSEditions = @()
@@ -62,7 +62,9 @@
     # NestedModules = @()
 
     # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-    FunctionsToExport = @()
+    FunctionsToExport = @(
+        'Build-Module'
+    )
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
     CmdletsToExport = @()
---
+++ Builder/public/Build-Module.ps1
@@ -0,0 +1,48 @@
+function Build-Module {
+    param (
+        # Name of Plaster template to use
+        [ValidateSet('BasicModule')]
+        $TemplateName = 'BasicModule',
+
+        # New-ModuleManifest parameter
+        [Parameter(Mandatory)]
+        $ModuleName,
+
+        # Filepath of new module
+        $Destination = ($env:PSModulePath -split ';')[0],
+
+        # New-ModuleManifest parameter
+        $Description,
+
+        # New-ModuleManifest parameter
+        $Version = '0.0.0',
+
+        # New-ModuleManifest parameter
+        $Author,
+
+        # New-ModuleManifest parameter
+        $CompanyName,
+
+        # New-ModuleManifest parameter
+        $PowerShellVersion
+    )
+
+    $PlasterParams = @{
+        # Plaster
+        TemplatePath = Join-Path -Path "$PSScriptRoot\..\templates" -ChildPath $TemplateName
+        DestinationPath = Join-Path $Destination -ChildPath $ModuleName
+
+        # Module manifest fields
+        ModuleName = $ModuleName
+        ModuleDescription = $Description
+        ModuleVersion = $Version
+        ModuleAuthor = $Author
+        CompanyName = $CompanyName
+        PowerShellVersion = $PowerShellVersion
+    }
+
+    Write-Host -ForegroundColor Cyan "Building module [$ModuleName] at [$DestinationPath] using template [$TemplateName]"
+    Write-Host -ForgeoundColor DarkCyan "Using build configuration:`n`n$(ConvertTo-Json $PlasterParams)"
+
+    Invoke-Plaster @PlasterParams
+}
