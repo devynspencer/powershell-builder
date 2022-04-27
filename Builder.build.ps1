377d2cd7681155c18fa9861e1af2a296b8b2cf30
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-04-27 02:02:10 -0700

    Move packaging tasks into separate file

--- Builder.build.ps1
+++ Builder.build.ps1
@@ -37,24 +37,6 @@ Enter-Build {
     requires -Variable Manifest, Config
 }
 
-# Synopsis: Publish module to local staging repository
-task Package Clean, {
-    $script:PackagePath = Join-Path -Path $BuildRoot -ChildPath $Config.StagingPath
-    $script:PackageFileName = @($ModuleName, $Manifest.ModuleVersion, 'nupkg') -join '.'
-
-    # Staging locally allows non-standard attributes to be added to the nuspec,
-    # and is currently necessary to publish to GitHub Packages
-    # - See @cdhunt's response here: https://github.com/PowerShell/PowerShellGet/issues/163
-    $PublishParams = @{
-        Repository = $Config.StagingRepositoryName
-        Path = "$BuildRoot\$($ModuleName)"
-        NuGetApiKey = $Config.RegistryToken
-    }
-
-    Publish-Module @PublishParams
-    Write-Build Cyan "Building package [$PackageFileName] in [$($Config.StagingPath)]"
-}
-
 # Synopsis: Perform all publishing tasks
 task Publish Package, {
     $script:RegistryUri = "$($Config.RegistryBaseUri)\$($Config.ProjectName)"
