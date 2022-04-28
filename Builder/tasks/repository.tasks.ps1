56ec418055ddfcd36ad8d93ba1d6b0716459d858
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-04-28 16:59:25 -0700

    Move repository tasks into repository tasks file

--- Builder/tasks/repository.tasks.ps1
+++ Builder/tasks/repository.tasks.ps1
@@ -1,3 +1,38 @@
+# Synopsis: Unregister repositories and package sources
+task Unregister {
+    Write-Build Cyan 'Unregistering PowerShellGet repositories and package sources ...'
+
+    foreach ($Registered in @($Config.RepositoryName, $Config.StagingRepositoryName)) {
+        Unregister-PSRepository -Name $Registered -ErrorAction 0 | Out-Null
+        Unregister-PackageSource -Source $Registered -ErrorAction 0 | Out-Null
+    }
+}
+
+# Synopsis: Register repositories and package sources
+task Register Unregister, Credentials, {
+    # Staging locally allows non-standard attributes to be added to the nuspec,
+    # and is currently necessary to publish to GitHub Packages
+    # - See @cdhunt's response here: https://github.com/PowerShell/PowerShellGet/issues/163
+
+    Write-Build Cyan 'Registering PowerShellGet repositories and package sources ...'
+
+    $StagingParams = @{
+        Name = $Config.StagingRepositoryName
+        SourceLocation = $Config.StagingPath
+    }
+
+    Register-PSRepository @StagingParams -InstallationPolicy 'Trusted'
+
+    $RegistryParams = @{
+        Name = $Config.RepositoryName
+        SourceLocation = $Config.RegistryIndexUri
+        PublishLocation = $Config.RegistryBaseUri
+        Credential = $RegistryCredential
+    }
+
+    Register-PSRepository @RegistryParams -InstallationPolicy 'Trusted'
+}
+
 # Synopsis: Register internal PSRepository
 task RegisterOrgRepo {
     $RegisterParams = @{
