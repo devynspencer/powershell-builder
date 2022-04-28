1a36735185a052e5b8b434d7f73902e38f6ebbad
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-04-28 16:09:27 -0700

    Set build environment explicitly

--- Builder.build.ps1
+++ Builder.build.ps1
@@ -21,6 +21,57 @@ param (
 
 Set-StrictMode -Version Latest
 
+# Configure build environment
+Set-BuildEnvironment -Force
+
+$ModuleVersion = (Import-PowerShellDataFile -Path $env:BHPSModuleManifest).ModuleVersion
+
+$BuilderEnv = [ordered] @{
+    General = @{
+        # Root directory for the project
+        ProjectRoot = $env:BHProjectPath
+
+        # Root directory for the module
+        SrcRootDir = $env:BHPSModulePath
+
+        # The name of the module. This should match the basename of the PSD1 file
+        ModuleName = $env:BHProjectName
+
+        # Module version
+        ModuleVersion = $moduleVersion
+
+        # Module manifest path
+        ModuleManifestPath = $env:BHPSModuleManifest
+    }
+
+    Build = @{
+        # Output directory for module build
+        OutDir = [IO.Path]::Combine($env:BHProjectPath, 'temp', 'output')
+
+        # Directory to write build logs to
+        LogDir = [IO.Path]::Combine($env:BHProjectPath, 'temp', 'logs')
+
+        # Directory to save reports to
+        ReportDir = [IO.Path]::Combine($env:BHProjectPath, 'temp', 'reports')
+    }
+
+    Test = @{
+
+    }
+
+    Help = @{
+
+    }
+
+    Docs = @{
+
+    }
+
+    Publish = @{
+
+    }
+}
+
 # Load build tasks from separate module
 # https://github.com/nightroman/Invoke-Build/tree/master/Tasks/Import
 Import-Module Builder -Force
