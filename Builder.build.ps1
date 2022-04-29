9c65303a650f1e8944cc135c44f6194dc499e89b
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-04-28 17:47:02 -0700

    Remove unused parameters

--- Builder.build.ps1
+++ Builder.build.ps1
@@ -1,24 +1,5 @@
 #requires -modules InvokeBuild, BuildHelpers
 
-<#
-.SYNOPSIS
-    Executes build tasks within a workspace.
-
-.DESCRIPTION
-    A portable collection of build tasks, executed by `Invoke-Build ...` within the project root. Tasks are
-    executed using the InvokeBuild module, see: https://github.com/nightroman/Invoke-Build.
-
-
-.EXAMPLE
-    Invoke-Build -ConfigurationFile ./Configuration.psd1
-
-    Executes a build process, using the project configuration file.
-#>
-param (
-    [ValidateScript({ Test-Path $_ })]
-    $ConfigurationFile = './Configuration.psd1'
-)
-
 Set-StrictMode -Version Latest
 
 # Configure build environment
